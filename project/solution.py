592088c3fca866078ff394d0a71a9efd7ee7260c
dagak <dkrenich@student.agh.edu.pl>
2026-02-18 17:41:47 +0100

    Added functions and tests

---
+++ project/solution.py
@@ -0,0 +1,103 @@
+import pandas as pd
+import re
+
+'''
+This function should chceck wether the column name is valid. 
+Column name is valid if :
+- is not empty
+- contains only letters and underscores
+'''
+def is_valid_column_name(column_name: str) -> bool:
+    if not column_name:
+        return False 
+    pattern = r"^[A-Za-z_]+$"
+    return re.fullmatch(pattern, column_name) is not None
+        
+'''
+This function checks whether the rule is correct. 
+It is, when:
+- Role contains only : +, -, *, _
+- Role may contain spaces and letters
+'''
+def is_valid_role(role: str)->bool:
+    if not isinstance(role, str) or not role.strip():
+        return False 
+    pattern = r"^[A-Za-z_+\-* ]+$"
+    return re.fullmatch(pattern, role) is not None 
+
+'''
+This function extracts column1, column2 and operator from role.
+'''
+def parse_role(role: str) -> tuple | None:
+    role = role.replace(" ", "")
+    operators = ["*", "-", "+"]
+    is_operator = []
+    
+    for i in role:
+        if i in operators:
+            is_operator.append(i)
+    # if more than one operator: 
+    if len(is_operator) != 1:
+        return None
+    
+    # split two columns by operator
+    op = is_operator[0]
+    columns = role.split(op)
+    
+    if len(columns)!=2:
+        return None 
+    left_column = columns[0]
+    right_column = columns[1]
+    if not left_column or not right_column:
+        return None 
+    
+    return left_column, right_column, op
+
+
+def add_virtual_column(df: pd.DataFrame, role: str, new_column: str) -> pd.DataFrame:
+    # Validation
+
+    #Type 
+    if not isinstance(df, pd.DataFrame):
+        return pd.DataFrame()
+    #New Column name 
+    if not is_valid_column_name(new_column):
+        return pd.DataFrame()
+    #All columns
+    for column in df.columns:
+        if not is_valid_column_name(column):
+            return pd.DataFrame()
+    #Role
+    if not is_valid_role(role):
+        return pd.DataFrame()
+    
+    parsed_role = parse_role(role)
+    if parsed_role == None:
+        return pd.DataFrame()
+    left_role, right_role, operator = parsed_role 
+    
+    if left_role not in df.columns or right_role not in df.columns:
+        return pd.DataFrame()
+
+    # Creating new column
+    new_df = df.copy()
+    if operator == "+":
+        new_df[new_column] = new_df[left_role] + new_df[right_role]
+    
+    elif operator == '-':
+        new_df[new_column] = new_df[left_role] - new_df[right_role]
+    
+    elif operator == "*":
+        new_df[new_column] = new_df[left_role] * new_df[right_role]
+    
+    return new_df
+
+# Testing by hand 
+# if __name__ == "__main__":
+#     result_col = is_valid_column_name("test")
+    
+#     result_pr = is_valid_role("test1 + tst2")
+#     print(result_pr)
+
+
+    
---
+++ project/test_solution.py
@@ -0,0 +1,71 @@
+import pandas as pd
+from solution import add_virtual_column
+
+
+def test_sum_of_two_columns():
+    df = pd.DataFrame([[1, 1]] * 2, columns = ["label_one", "label_two"])
+    df_expected = pd.DataFrame([[1, 1, 2]] * 2, columns = ["label_one", "label_two", "label_three"])
+    df_result = add_virtual_column(df, "label_one+label_two", "label_three")
+    assert df_result.equals(df_expected), f"The function should sum the columns: label_one and label_two.\n\nResult:\n\n{df_result}\n\nExpected:\n\n{df_expected}"
+
+
+def test_multiplication_of_two_columns():
+    df = pd.DataFrame([[1, 1]] * 2, columns = ["label_one", "label_two"])
+    df_expected = pd.DataFrame([[1, 1, 1]] * 2, columns = ["label_one", "label_two", "label_three"])
+    df_result = add_virtual_column(df, "label_one * label_two", "label_three")
+    assert df_result.equals(df_expected), f"The function should multiply the columns: label_one and label_two.\n\nResult:\n\n{df_result}\n\nExpected:\n\n{df_expected}"
+
+
+def test_subtraction_of_two_columns():
+    df = pd.DataFrame([[1, 1]] * 2, columns = ["label_one", "label_two"])
+    df_expected = pd.DataFrame([[1, 1, 0]] * 2, columns = ["label_one", "label_two", "label_three"])
+    df_result = add_virtual_column(df, "label_one - label_two", "label_three")
+    assert df_result.equals(df_expected), f"The function should subtract the columns: label_one and label_two.\n\nResult:\n\n{df_result}\n\nExpected:\n\n{df_expected}"
+
+
+def test_empty_result_when_invalid_labels():
+    df = pd.DataFrame([[1, 2]] * 3, columns = ["label_one", "label_two"])
+    df_result = add_virtual_column(df, "label_one + label_two", "label3")
+    assert df_result.empty, f"Should return an empty df when the \"new_column\" is invalid.\n\nResult:\n\n{df_result}\n\nExpected:\n\nEmpty df"
+
+
+def test_empty_result_when_invalid_rules():
+    df = pd.DataFrame([[1, 1]] * 2, columns = ["label_one", "label_two"])
+    df_result = add_virtual_column(df, "label&one + label_two", "label_three")
+    assert df_result.empty, f"Should return an empty df when the role have invalid character: '&'.\n\nResult:\n\n{df_result}\n\nExpected:\n\nEmpty df"
+    df_result = add_virtual_column(df, "label_five + label_two", "label_three")
+    assert df_result.empty, f"Should return an empty df when the role have a column which isn't in the df: 'label_five'.\n\nResult:\n\n{df_result}\n\nExpected:\n\nEmpty df"
+
+
+def test_when_extra_spaces_in_rules():
+    df = pd.DataFrame([[1, 1]] * 2, columns = ["label_one", "label_two"])
+    df_expected = pd.DataFrame([[1, 1, 2]] * 2, columns = ["label_one", "label_two", "label_three"])
+    df_result = add_virtual_column(df, "label_one + label_two ", "label_three")
+    assert df_result.equals(df_expected), f"Should work when the role have spaces between the operation and the column.\n\nResult:\n\n{df_result}\n\nExpected:\n\n{df_expected}"
+    df_result = add_virtual_column(df, "  label_one + label_two ", "label_three")
+    assert df_result.equals(df_expected), f"Should work when the role have extra spaces in the start/end.\n\nResult:\n\n{df_result}\n\nExpected:\n\n{df_expected}"
+
+# Own tests 
+def test_invalid_when_multiple_operators():
+    df = pd.DataFrame([[1, 2]], columns=["a", "b"])
+    result = add_virtual_column(df, "a+b-c", "c")
+    assert result.empty
+
+def test_invalid_when_no_operator():
+    df = pd.DataFrame([[1, 2]], columns=["a", "b"])
+    result = add_virtual_column(df, "ab", "c")
+    assert result.empty
+
+def test_invalid_when_column_not_exists():
+    df = pd.DataFrame([[1, 2]], columns=["a", "b"])
+    result = add_virtual_column(df, "a+c", "d")
+    assert result.empty
+
+def test_invalid_when_empty_role():
+    df = pd.DataFrame([[1, 2]], columns=["a", "b"])
+    result = add_virtual_column(df, "   ", "c")
+    assert result.empty
+
+def test_invalid_when_df_not_dataframe():
+    result = add_virtual_column("not a df", "a+b", "c")
+    assert result.empty
